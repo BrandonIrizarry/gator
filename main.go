1ded9f672ab82ba966dba25033085d324a9e8a25
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-02-12 11:21:18 -0500

    Enable setting the current user with the 'login' command
    This now shows that our command-registration-based architecture is
    online and works.

--- main.go
+++ main.go
@@ -9,25 +9,42 @@ import (
 const configBasename = ".gatorconfig.json"
 
 func main() {
+	// First, acquire the State the configuration file fullpath.
 	homeDir, err := os.UserHomeDir()
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
 	}
 
 	state := configuration.State{
 		ConfigFile: fmt.Sprintf("%s/%s", homeDir, configBasename),
 	}
 
+	// Read the current JSON configuration into the State.
 	if err := configuration.Read(&state); err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
 	}
 
-	configuration.SetUser(&state, "ted")
-
-	if err := configuration.Read(&state); err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+	// Parse the current command, and check if everything is OK.
+	if len(os.Args) <= 1 {
+		fmt.Fprintf(os.Stderr, "No arguments provided\n")
+		os.Exit(1)
 	}
 
-	fmt.Printf("%v\n%v\n", *state.Config, state.ConfigFile)
+	commandName := os.Args[1]
+	command, err := configuration.GetCommand(commandName)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	// Invoke the given command.
+	if err = command(&state, os.Args[2:]...); err != nil {
+		fmt.Fprintf(os.Stderr, "In command '%s': %v\n", commandName, err)
+		os.Exit(1)
+	}
+
 }
