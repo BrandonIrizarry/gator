3f97ae8b073f26960ea7ac757de149d8966d5952
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-02-19 19:03:49 -0500

    Move 'feeds' table creation to its own migration file

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -6,15 +6,5 @@ CREATE TABLE users(
        name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE feeds(
-       id UUID PRIMARY KEY,
-       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-       updated_at TIMESTAMP NOT NULL,
-       name TEXT NOT NULL, -- the name/title of the given RSS feed
-       url TEXT UNIQUE NOT NULL,
-       user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
-);
-
 -- +goose Down
-DROP TABLE feeds;
 DROP TABLE users;
