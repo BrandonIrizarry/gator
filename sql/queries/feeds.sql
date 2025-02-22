5096c809f10edcaba6c28d429393a448c7a1c08e
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-02-21 19:18:05 -0500

    Add 'GetNextFeedToFetch' query
    Another good name for this might be, "FetchStaleFeed".

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -23,3 +23,7 @@ UPDATE feeds
 SET last_fetched_at = CURRENT_TIMESTAMP,
     updated_at = CURRENT_TIMESTAMP
 WHERE feeds.id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at NULLS FIRST;
